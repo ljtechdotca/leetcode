628bce0dc054d69aaac928391b2c181c40684fb0
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2023-07-19 08:57:35 -0700

    Update addTwoNumbers.js

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -12,9 +12,9 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2, memo = 0) {
-    if (!l1 && !l2 && !memo) return null;
-    const val = (l1?.val || 0) + (l2?.val || 0) + memo;
+var addTwoNumbers = function(l1, l2, carry = 0) {
+    if (!l1 && !l2 && !carry) return null;
+    const val = (l1?.val || 0) + (l2?.val || 0) + carry;
     const node = addTwoNumbers(l1?.next, l2?.next, Math.floor(val / 10));
     return new ListNode(val % 10, node);
 };
