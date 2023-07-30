10e59ed57739bfbdbc9488d299bc8e7b53c992cc
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2023-07-30 12:02:49 -0700

    Create 4-median-of-two-sorted-arrays.js

---
+++ 4-median-of-two-sorted-arrays.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let nums = [...nums1, ...nums2].sort((a, b) => a - b);
+    let mid = Math.floor(nums.length / 2);
+    
+    return nums.length % 2 != 0 ? nums[mid]: (nums[mid - 1] + nums[mid]) / 2;
+};
