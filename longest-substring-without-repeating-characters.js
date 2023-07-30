a38e261496574ccf403bbb2ebc74adfbcd5ec7aa
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2023-07-30 11:41:45 -0700

    Create longest-substring-without-repeating-characters.js

---
+++ longest-substring-without-repeating-characters.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if (s.length < 2) return s.length;
+
+    let set = new Set();
+    let left = 0;
+    let size = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        while (set.has(s[i])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[i]);
+        size = Math.max(size, i - left + 1);
+    }
+
+    return size;
+};
