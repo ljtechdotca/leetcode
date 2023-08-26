33b4082e470d7d043c7f39e91c149eaaca630b36
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2023-08-26 12:51:53 -0700

    Create 5-longest-palindromic-substring.js

---
+++ 5-longest-palindromic-substring.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    const checkPalindrome = (s, i, j) => {
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    let longest = 0;
+    let pointer = 0;
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i; j < s.length; j++) {
+            if (checkPalindrome(s, i, j)) {
+                if (j - i + 1 > longest) {
+                    longest = j - i + 1;
+                    pointer = i;
+                }
+            }
+        }
+    }
+    
+    return s.substring(pointer, pointer + longest);
+};
