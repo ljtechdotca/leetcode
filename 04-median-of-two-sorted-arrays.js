bdc7dd470f8a7c8a5c5c00bba5882106095a0d59
Landon Johnson <59711105+ljtechdotca@users.noreply.github.com>
2023-10-11 09:01:12 -0700

    Update 04-median-of-two-sorted-arrays.js

--- 04-median-of-two-sorted-arrays.js
+++ 04-median-of-two-sorted-arrays.js
@@ -8,6 +8,5 @@
 var findMedianSortedArrays = function(nums1, nums2) {
     let nums = [...nums1, ...nums2].sort((a, b) => a - b);
     let mid = Math.floor(nums.length / 2);
-    
     return nums.length % 2 != 0 ? nums[mid]: (nums[mid - 1] + nums[mid]) / 2;
 };
